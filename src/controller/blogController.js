db6a552b7ebee101daaa5d070fe7564dddaf1b37
ilkay <06ilkayozturk@gmail.com>
2023-10-12 11:52:13 +0300

    blogRoute

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -8,4 +8,29 @@ require("express-async-errors");
 // Call Models:
 const { BlogPost } = require("../models/blogModel");
 
-module.exports.BlogPost = {};
+module.exports.BlogPost = {
+  list: async (req, res) => {
+    const data = await BlogPost.find();
+
+    res.status(200).send({
+      error: false,
+      result: data,
+    });
+  },
+
+  create: async (req, res) => {
+    const data = await BlogPost.insertOne(req.body);
+
+    res.status(201).send({
+      error: false,
+      body: req.body,
+      result: data,
+    });
+  },
+
+  read: async (req, res) => {},
+
+  update: async (req, res) => {},
+
+  delete: async (req, res) => {},
+};
--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -2,3 +2,18 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+const router = require("express").Router();
+// Call Controllers:
+const { BlogPost } = require("../controller/blogController");
+//----------
+//BlogPost
+//----------
+Router.route("/post").get(BlogPost.list).post(BlogPost.create);
+
+Router.route("/post/:postId")
+  .get(BlogPost.read)
+  .put(BlogPost.update)
+  .delete(BlogPost.delete);
+
+module.exports = router;
