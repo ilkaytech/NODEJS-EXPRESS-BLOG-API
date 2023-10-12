ab10220838ff30d474d97774b731c63533353c06
ilkay <06ilkayozturk@gmail.com>
2023-10-12 11:31:20 +0300

    blogController:call_models

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -2,3 +2,10 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+// Catch async-errors and send to errorHandler:
+require("express-async-errors");
+
+// Call Models:
+const { BlogPost } = require("../models/blogModel");
+
+module.exports.BlogPost = {};
--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -5,34 +5,72 @@
 
 const mongoose = require("mongoose");
 
-const nameSchema = new mongoose.Schema(
+// const nameSchema = new mongoose.Schema(
+//   {
+//     //_id: AUTO CREATED
+//     fieldName: {
+//       type: String,
+//       default: null,
+//       trim: true, // trim(data)
+//       select: true, //Show/UnShow
+//       index: true,
+//       unique: false,
+//       required: [true, "Error-Message"],
+//       enum: [[0, 1, 2, 3], "Error-Message"],
+//       validate: [
+//         function (data) {
+//           return true;
+//         },
+//         "Error-Message",
+//       ],
+//       get: function (data) {
+//         return true;
+//       },
+//       set: function (data) {
+//         return true;
+//       },
+//     },
+//   },
+//   {
+//     collection: "collectionName",
+//     timestamps: true, //Create and Manage 'createdAt' and 'updatedAt'
+//   }
+// );
+
+/*------------------------------------------------------------*/
+
+const blogPostSchema = new mongoose.Schema(
   {
-    //_id: AUTO CREATED
-    fieldName: {
+    // _id
+    title: {
       type: String,
-      default: null,
-      trim: true, // trim(data)
-      select: true, //Show/UnShow
-      index: true,
-      unique: false,
-      required: [true, "Error-Message"],
-      enum: [[0, 1, 2, 3], "Error-Message"],
-      validate: [
-        function (data) {
-          return true;
-        },
-        "Error-Message",
-      ],
-      get: function (data) {
-        return true;
-      },
-      set: function (data) {
-        return true;
-      },
+      trim: true,
+      require: true,
     },
+
+    content: {
+      type: String,
+      trim: true,
+      require: true,
+    },
+
+    published: {
+      type: Boolean,
+      default: true,
+    },
+
+    // createdAt
+    // updatedAt
   },
   {
-    collection: "collectionName",
-    timestamps: true, //Create and Manage 'createdAt' and 'updatedAt'
+    collection: "blogPosts",
+    timestamps: true,
   }
 );
+
+// const BlogPostModel = mongoose.model("blogPost", blogPostSchema);
+
+module.exports = {
+  // BlogCategory:
+  BlogPost: mongoose.model("BlogPost", blogPostSchema),
+};
